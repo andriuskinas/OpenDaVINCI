227dd99f0a38aaa73654ac2acdbae75daca5e7c9
andriuskinas <andrius.murnikovas@gmail.com>
2016-08-10 06:48:24 +0200

    Update BoxParker.cpp

--- automotive/miniature/boxparker/src/BoxParker.cpp
+++ automotive/miniature/boxparker/src/BoxParker.cpp
@@ -18,36 +18,27 @@
  */
 
 #include <iostream>
-
 #include "opendavinci/odcore/data/Container.h"
-
 #include "opendavinci/GeneratedHeaders_OpenDaVINCI.h"
 #include "automotivedata/GeneratedHeaders_AutomotiveData.h"
-
 #include "BoxParker.h"
 
 namespace automotive {
     namespace miniature {
-
         using namespace std;
         using namespace odcore::base;
         using namespace odcore::base::module;
         using namespace odcore::data;
         using namespace automotive;
 
-        BoxParker::BoxParker(const int32_t &argc, char **argv) :
+        BoxParker::BoxParker(const int32_t &argc, char **argv) :   
             TimeTriggeredConferenceClientModule(argc, argv, "BoxParker"),
             m_foundGaps() {}
 
-        BoxParker::~BoxParker() {}
+            BoxParker::~BoxParker() {}
 
-        void BoxParker::setUp() {
-            // This method will be call automatically _before_ running body().
-        }
-
-        void BoxParker::tearDown() {
-            // This method will be call automatically _after_ return from body().
-        }
+            void BoxParker::setUp() {}
+            void BoxParker::tearDown() {}
 
         vector<double> BoxParker::getFoundGaps() const {
             return m_foundGaps;
@@ -55,14 +46,11 @@ namespace automotive {
 
         // This method will do the main data processing job.
         odcore::data::dmcp::ModuleExitCodeMessage::ModuleExitCode BoxParker::body() {
-            double distanceOld = 0;
-            double absPathStart = 0;
-            double absPathEnd = 0;
-
+           
             int stageMoving = 0;
-            int stageMeasuring = 0;
+            
 
-            while (getModuleStateAndWaitForRemainingTimeInTimeslice() == odcore::data::dmcp::ModuleStateMessage::RUNNING) {
+        while (getModuleStateAndWaitForRemainingTimeInTimeslice() == odcore::data::dmcp::ModuleStateMessage::RUNNING) {
                 // 1. Get most recent vehicle data:
                 Container containerVehicleData = getKeyValueDataStore().get(automotive::VehicleData::ID());
                 VehicleData vd = containerVehicleData.getData<VehicleData> ();
@@ -74,92 +62,93 @@ namespace automotive {
                 // Create vehicle control data.
                 VehicleControl vc;
 
-                // Moving state machine.
-                if (stageMoving == 0) {
-                    // Go forward.
-                    vc.setSpeed(1);
-                    vc.setSteeringWheelAngle(0);
-                }
-                if ((stageMoving > 0) && (stageMoving < 20)) {
-                    // Move slightly forward.
-                    vc.setSpeed(1);
-                    vc.setSteeringWheelAngle(0);
-                    stageMoving++;
-                }
-                if ((stageMoving >= 20) && (stageMoving < 25)) {
-                    // Stop.
-                    vc.setSpeed(0);
-                    vc.setSteeringWheelAngle(0);
-                    stageMoving++;
-                }
-                if ((stageMoving >= 25) && (stageMoving < 80)) {
-                    // Backwards, steering wheel to the right.
-                    vc.setSpeed(-2);
-                    vc.setSteeringWheelAngle(25);
-                    stageMoving++;
-                }
-                if (stageMoving >= 80) {
-                    // Stop.
-                    vc.setSpeed(0);
-                    vc.setSteeringWheelAngle(0);
+                //Initalize the back and front sensors          
+                //double BACK_SENSOR = sbd.getValueForKey_MapOfDistances(2);
+                //double FRONT_SENSOR = sbd.getValueForKey_MapOfDistances(3); 
 
+                //IR sensor
+               // double RIGHT_SENSOR = sbd.getValueForKey_MapOfDistances(0);
+                double DISTANCE_CAR = vd.getAbsTraveledPath(); 
+
+                cout << "distance = " << DISTANCE_CAR << endl; 
+       
+        if ((DISTANCE_CAR >= 0) && (DISTANCE_CAR < 88)) { 
+                    //Go forward.
+                    cout << "MOVING FORWARD " << endl;
+                    //vc.setSteeringWheelAngle(0);
+                    vc.setSpeed(2);
+
+                }
+                if ((DISTANCE_CAR > 33) && (DISTANCE_CAR < 41.5)) {
+                    // TURNING RIGHT UNTIL DISTANCE 41.5.
+                    cout << "TURNING RIGHT UNTIL DISTANCE 41.5 " << endl;
+                    vc.setSpeed(1);
+vc.setSpeed(1);
+                    vc.setSteeringWheelAngle(45);
                     stageMoving++;
                 }
-                if (stageMoving >= 150) {
+
+if (( DISTANCE_CAR>= 50) && (DISTANCE_CAR < 57))  {
+                    // DRIVES FORWARD.
+                    cout << "DRIVING" << endl;
+                   // vc.setSpeed(1);
+                    vc.setSpeed(1);
+                    vc.setSteeringWheelAngle(-100);
+                    stageMoving++;
+                }
+
+if (( DISTANCE_CAR>= 58.5) && (DISTANCE_CAR < 66.5))  {
+                    // TURNS INTO PARKING SLOT.
+                    cout << "TURNING INTO PARKING LEFT" << endl;
+                    vc.setSpeed(1);
+                    vc.setSpeed(1);
+                    vc.setSteeringWheelAngle(80);
+                    stageMoving++;
+                }
+
+if (( DISTANCE_CAR>= 72) && (DISTANCE_CAR < 80))  {
+                    // DRIVES FORWARD.
+                    cout << "DRIVING" << endl;
+                   // vc.setSpeed(1);
+                    vc.setSpeed(1);
+                    vc.setSteeringWheelAngle(100);
+                    stageMoving++;
+                }
+
+ if ((DISTANCE_CAR >= 86.2) && (DISTANCE_CAR < 95.7))  {
+                    // TURNS RIGHT.
+                    cout << "TURNING RIGHT" << endl;
+                    vc.setSpeed(-1);
+vc.setSpeed(-1);
+                    vc.setSteeringWheelAngle(-30);
+                    stageMoving++;
+                }
+
+if ((DISTANCE_CAR >= 95.7) && (DISTANCE_CAR < 97.7)) { 
+                    //GO BACKWARD.
+                    cout << "TURNING BACKWARDS" << endl;
+                    //vc.setSteeringWheelAngle(0);
+                    vc.setSpeed(-1);
+
+                }
+
+ if (DISTANCE_CAR >= 98) {
                     // End component.
-                    break;
-                }
+                    cout << "PARKED" << endl;
+                    vc.setSpeed(0);
+                    stageMoving++;
+                    //break;
+                } 
 
-                // Measuring state machine.
-                switch (stageMeasuring) {
-                    case 0:
-                        {
-                            // Initialize measurement.
-                            distanceOld = sbd.getValueForKey_MapOfDistances(2);
-                            stageMeasuring++;
-                        }
-                    break;
-                    case 1:
-                        {
-                            // Checking for distance sequence +, -.
-                            if ((distanceOld > 0) && (sbd.getValueForKey_MapOfDistances(2) < 0)) {
-                                // Found distance sequence +, -.
-                                stageMeasuring = 2;
-                                absPathStart = vd.getAbsTraveledPath();
-                            }
-                            distanceOld = sbd.getValueForKey_MapOfDistances(2);
-                        }
-                    break;
-                    case 2:
-                        {
-                            // Checking for distance sequence -, +.
-                            if ((distanceOld < 0) && (sbd.getValueForKey_MapOfDistances(2) > 0)) {
-                                // Found distance sequence -, +.
-                                stageMeasuring = 1;
-                                absPathEnd = vd.getAbsTraveledPath();
 
-                                const double GAP_SIZE = (absPathEnd - absPathStart);
-                                cerr << "Size = " << GAP_SIZE << endl;
-                                m_foundGaps.push_back(GAP_SIZE);
-
-                                if ((stageMoving < 1) && (GAP_SIZE > 3.5)) {
-                                    stageMoving = 1;
-                                }
-                            }
-                            distanceOld = sbd.getValueForKey_MapOfDistances(2);
-                        }
-                    break;
-                }
-
+               
+                    
                 // Create container for finally sending the data.
                 Container c(vc);
                 // Send container.
                 getConference().send(c);
             }
-
             return odcore::data::dmcp::ModuleExitCodeMessage::OKAY;
         }
-
     } // miniature
 } // automotive
-
